a3543277de16178aea2d91c5147d0b1a00abe9a7
jakejg <git@stackblitz.com>
2024-12-23 09:22:14 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# reverse-sentence
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/jakejg/reverse-sentence)
---
+++ script.js
@@ -0,0 +1,27 @@
+let sentence = prompt('Enter a sentence to reverse word by word:');
+// let sentence = 'Test sentence';
+
+// Split the sentence into an array of words
+let words = sentence.split(' ');
+console.log({ words });
+
+// create a new array to hold the reversed sentence
+let reversedSentence = [];
+
+// loop over the array and reverse each word
+for (const word of words) {
+  // create an array of characters
+  const characters = word.split('');
+  // reverse the array
+  const reversedChars = characters.reverse();
+  // smush the array back into a string
+  const reversedWord = reversedChars.join('');
+  // add the reversed word to the new array
+  reversedSentence.push(reversedWord);
+}
+
+// Join the reversed array of words back into a sentence
+let result = reversedSentence.join(' '); // Pay attention to the blank space in the string in the join here.
+
+// Display the result
+console.log('Reversed sentence: ' + result);
